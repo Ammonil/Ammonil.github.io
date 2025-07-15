1fd81ed71532230ae415b0dd403cf913a1c46bba
Ammonil <37168115+Ammonil@users.noreply.github.com>
2025-07-14 22:59:27 -0400

    Lesson 12

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub for fetching grade data from the database
+function fetchGradeData() {
+  console.log("fetchGradeData() called");
+  // Future implementation will go here
+}
+
+// Stub for populating the gradebook table with data
+function populateGradebook(data) {
+  console.log("populateGradebook() called with data:", data);
+  // Future implementation will go here
+}
+
+// Call stubs to verify console output
+fetchGradeData();
+populateGradebook([]);
